82efb13445892e124518cc346d1736b564841ab3
Sarah Cowgill <sarah.cowgill@yahoo.com>
2020-07-06 14:08:59 -0700

    updating files

--- game.js
+++ game.js
@@ -23,16 +23,16 @@ function checkSecond(sec) {
     if (sec < 10 && sec >= 0) { sec = "0" + sec };
     if (sec < 0) { sec = "59" };
     return sec;
-}
+};
 
-var question = document.getElementById("question");
-var choices = Array.from(document.getElementsByClassName("choice-text"));
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
 
-var currentQuestion = {};
-var acceptingAnswers = true;
-var score = 0;
-var questionCounter = 0;
-var availableQuestions = [];
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
 var questions = [
     {
@@ -93,24 +93,14 @@ getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter ≥ MAX_QUESTIONS) {
         //go to end page
         return window.location.assign("end.html");
-
-    }};
-    
-    questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
-
-    choices.forEach(choice => {
-        const number = choice.dataset["number"];
-        choice.innerText = currentQuestion["choice" + number];
-    });
-    availableQuestions.splice(questionIndex, 1);
-
-    acceptingAnswers = true;
-
+    }
 
 };
+questionCounter++;
+const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+currentQuestion = availableQuestions[questionIndex];
+question.innerText = currentQuestion.question;
+
 
 choices.forEach(choice => {
     choice.addEventListener("click", e => {
