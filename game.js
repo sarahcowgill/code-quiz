65b2fb10792b66b77a6057b72d92696fd72e29ec
Sarah Cowgill <sarah.cowgill@yahoo.com>
2020-07-06 14:13:57 -0700

    updating files

--- game.js
+++ game.js
@@ -77,40 +77,45 @@ var questions = [
     }
 ];
 
+//CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 5;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions];
-    console.log(availableQuestions);
+    availableQuesions = [...questions];
     getNewQuestion();
-
 };
 
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter ≥ MAX_QUESTIONS) {
-        //go to end page
-        return window.location.assign("end.html");
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page
+        return window.location.assign('/end.html');
     }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;
 
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuesions.splice(questionIndex, 1);
+    acceptingAnswers = true;
 };
-questionCounter++;
-const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-currentQuestion = availableQuestions[questionIndex];
-question.innerText = currentQuestion.question;
 
-
-choices.forEach(choice => {
-    choice.addEventListener("click", e => {
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
         if (!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
-        console.log(selectedAnswer);
+        const selectedAnswer = selectedChoice.dataset['number'];
         getNewQuestion();
     });
 });
-startGame(); 
+
+startGame();
