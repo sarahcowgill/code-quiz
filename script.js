0c2336863f3af2b58a5cdcb2b651ef899a0723d3
Sarah Cowgill <sarah.cowgill@yahoo.com>
2020-07-06 12:51:33 -0700

    updating files

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function startTimer() {
     if (s == 59) { m = m - 1 }
     if (m < 0) {
         alert('Time is Up!');
-        return; 
+        return;
     }
 
     document.getElementById('timer').innerHTML =
@@ -26,4 +26,94 @@ function checkSecond(sec) {
 }
 
 var question = document.getElementById("question");
-var choices = document.getElementsByClassName("choice-text");
+var choices = Array.from(document.getElementsByClassName("choice-text"));
+
+var currentQuestion = {};
+var acceptingAnswers = true;
+var score = 0;
+var questionCounter = 0;
+var availableQuestions = [];
+
+var questions = [
+    {
+        question: "Inside the HTML which tag do we use for Javascript?",
+        choice1: "<javascript>",
+        choice2: "<style.js>",
+        choice3: "<script>",
+        answer: 3
+    },
+
+    {
+        question: "How do you write 'Hello!' in an alert box?",
+        choice1: "msgBox('Hello!');",
+        choice2: "alert('Hello!');",
+        choice3: "alertBox('Hello!');",
+        answer: 2
+
+    },
+
+    {
+        question: "The condition of an if/else statement is enclosed within ____.",
+        choice1: "quotes",
+        choice2: "curly brackets",
+        choice3: "parantheses",
+        answer: 3
+    },
+
+    {
+        question: "Commonly used data types DO NOT include:",
+        choice1: "alerts",
+        choice2: "strings",
+        choice3: "booleans",
+        answer: 1
+    },
+
+    {
+        question: "What is the correct syntax referring to the external script called 'xxx.js'?",
+        choice1: "<script href='xxx.js'>",
+        choice2: "<script src='xxx.js'>",
+        choice3: "<script name='xxx.js'>",
+        answer: 2
+    }
+];
+
+const CORRECT_BONUS = 10; 
+const MAX_QUESTIONS = 5; 
+
+startGame = () => {
+    questionCounter = 0; 
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+
+};
+
+getNewQuestion = () => {
+    questionCounter++; 
+   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+   currentQuestion = availableQuestions[questionIndex];
+   question.innerText = currentQuestion.question;
+
+   choices.forEach(choice => {
+       const number = choice.dataset["number"];
+       choice.innerText = currentQuestion["choice" + number];
+   });
+   availableQuestions.splice(questionIndex, 1);
+
+   acceptingAnswers = true; 
+
+};
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
+        
+        acceptingAnswers = false; 
+        const selectedChoice = e.target; 
+        const selectedAnswer = selectedChoice.dataset["number"];
+        console.log(selectedAnswer);
+        getNewQuestion();
+     });
+});
+startGame (); 
